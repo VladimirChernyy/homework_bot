6213a4b090983c9f54b608804790ae2eac8846e1
Vladimir Chernyy <Vladimir_chernyy@list.ru>
2023-05-05 15:00:28 +0400

    Fix token

--- homework.py
+++ homework.py
@@ -11,9 +11,9 @@ from exceptions import HTTPError
 
 load_dotenv()
 
-PRACTICUM_TOKEN = os.getenv('TOKEN')
-TELEGRAM_TOKEN = os.getenv('TELEGRAM')
-TELEGRAM_CHAT_ID = os.getenv('CHAT')
+PRACTICUM_TOKEN = os.getenv('PRACTICUM_TOKEN')
+TELEGRAM_TOKEN = os.getenv('TELEGRAM_TOKEN')
+TELEGRAM_CHAT_ID = os.getenv('TELEGRAM_CHAT_ID')
 
 RETRY_PERIOD = 600
 ENDPOINT = 'https://practicum.yandex.ru/api/user_api/homework_statuses/'
